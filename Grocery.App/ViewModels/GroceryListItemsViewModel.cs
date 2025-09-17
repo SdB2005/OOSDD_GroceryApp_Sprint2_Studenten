8120c95f271b30822dec4ddf25ee10bbfbe0dc89
SdB2005 <semdeboerdere@gmail.com>
2025-09-17 15:12:07 +0200

    available products functionaliteit

--- Grocery.App/ViewModels/GroceryListItemsViewModel.cs
+++ Grocery.App/ViewModels/GroceryListItemsViewModel.cs
@@ -37,7 +37,16 @@ namespace Grocery.App.ViewModels
             //Maak de lijst AvailableProducts leeg
             //Haal de lijst met producten op
             //Controleer of het product al op de boodschappenlijst staat, zo niet zet het in de AvailableProducts lijst
-            //Houdt rekening met de voorraad (als die nul is kun je het niet meer aanbieden).            
+            //Houdt rekening met de voorraad (als die nul is kun je het niet meer aanbieden).
+            AvailableProducts.Clear();
+            foreach (var product in _productService.GetAll())
+            {
+                if (product.Stock > 0 && !MyGroceryListItems.Any(x => x.ProductId == product.Id))
+                {
+                    AvailableProducts.Add(product);
+                }
+            }
+
         }
 
         partial void OnGroceryListChanged(GroceryList value)
